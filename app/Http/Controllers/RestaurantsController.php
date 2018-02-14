3715e94f60f4757dc16b786abf291bbb48bc67af
steelVII <jeremy@naxpansion.com>
2018-02-14 12:14:34 +0800

    Created Model, controller and migration files

--- app/Http/Controllers/RestaurantsController.php
+++ app/Http/Controllers/RestaurantsController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Restaurants;
 use Illuminate\Http\Request;
 
 class RestaurantsController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Restaurants  $restaurants
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurants $restaurants)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Restaurants  $restaurants
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Restaurants $restaurants)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Restaurants  $restaurants
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Restaurants $restaurants)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Restaurants  $restaurants
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Restaurants $restaurants)
+    {
+        //
+    }
 }
